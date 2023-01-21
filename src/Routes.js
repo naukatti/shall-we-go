2865ec59c53e4922881a4245ce1340075f07babe
naukatti <noora.peltok@gmail.com>
2023-01-21 19:42:43 +0200

    Formatting and accessibility changes

--- src/Routes.js
+++ src/Routes.js
@@ -1,48 +1,52 @@
 import React from "react";
 import { Query } from "react-apollo";
 import { gql } from "apollo-boost";
-import Itinerary from "./Itinerary"
+import Itinerary from "./Itinerary";
 import styled from "styled-components";
 
 // Home: lat:60.17786, lon:25.04901
 // Eficode: lat:60.1694107, lon:24.9236173
 const GET_ROUTES = gql`
-{
-  plan(numItineraries: 5, from: {lat:60.17786, lon:25.04901}, to: {lat: 60.1694107, lon: 24.9236173}) {
-    itineraries {
-      startTime
-      endTime
-      legs {
-        route {
-          shortName
-        }
-        from {
-          name
-          departureTime
-        }
-        to {
-          name
-          arrivalTime
-        }
-        trip {
-          tripHeadsign
+  {
+    plan(
+      numItineraries: 5
+      from: { lat: 60.17786, lon: 25.04901 }
+      to: { lat: 60.1694107, lon: 24.9236173 }
+    ) {
+      itineraries {
+        startTime
+        endTime
+        legs {
+          route {
+            shortName
+          }
+          from {
+            name
+            departureTime
+          }
+          to {
+            name
+            arrivalTime
+          }
+          trip {
+            tripHeadsign
+          }
         }
       }
     }
   }
-}
 `;
 
 const Table = styled.table`
   width: 100%;
   grid-template-columns: 5rem auto 5rem;
-`
+`;
 const TimeHeader = styled.th`
   text-align: left;
-`
+`;
 const HowToHeader = styled.th`
   text-align: center;
-`
+`;
 
 const Routes = () => (
   <Query query={GET_ROUTES}>
@@ -52,7 +56,15 @@ const Routes = () => (
 
       return (
         <div>
-          <h2> 🚌 TIMETABLES 🚆 </h2>
+          <h2>
+            <span role="img" aria-label="Bus emoji">
+              🚌
+            </span>
+            TIMETABLES
+            <span role="img" aria-label="Train emoji">
+              🚆
+            </span>
+          </h2>
           <Table>
             <thead>
               <TimeHeader>Leave home</TimeHeader>
@@ -60,7 +72,11 @@ const Routes = () => (
               <TimeHeader>At office</TimeHeader>
             </thead>
             <tbody>
-              {data && data.plan && data.plan.itineraries.map(itinerary => <Itinerary data={itinerary} />)}
+              {data &&
+                data.plan &&
+                data.plan.itineraries.map((itinerary) => (
+                  <Itinerary data={itinerary} />
+                ))}
             </tbody>
           </Table>
         </div>
